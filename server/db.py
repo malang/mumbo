b10500eb4fd5c59285a2be0bcfb7cfe5d450dc03
jibs <jibran@qubitdigital.com>
2011-09-24 02:12:05 +0100

    more initial code

---
+++ server/db.py
@@ -0,0 +1,19 @@
+# This is the email greeter service - you will need to get a message into here
+# which is then parsed and inserted into the db as appopriate
+
+# This is the db layer where we will make the db calls
+import pymongo
+from pymongo import Connection
+
+def _return_conn():
+    return pymongo.Connection().mumbo
+
+def write_thread(emobj):
+    db = _return_conn()
+    db.test.insert({"x": "y", "a": "b"})
+
+
+if __name__ == '__main__':
+    write_thread({})
+
+
---
+++ server/email_greeter.py
@@ -0,0 +1,32 @@
+# This is the email greeter service - you will need to get a message into here
+# which is then parsed and inserted into the db as appopriate
+
+import hashlib
+import simplejson as json
+
+exlude_filters = ['Re:', 'Fwd:']
+
+
+# this is what email obj should look like - ideally
+email_obj = {
+    'to': ['text@example.com', 'test2@example.com'], 
+    'from': ['me@example.com'], 
+    'subject' : 'Email subject line',
+    'content' : 'Content'
+    'thread_id': ''
+    }
+
+def process_subject(subj):
+    """gets raw subject and returns the normalized version"""
+    # todo: smarter normalization
+    return ''
+
+
+def process_email(email):
+    # todo: remove the re  / fwd stuff from the subj
+    cleaned_subj = process_subject(email['subject'])
+    subj_hash = 
+
+
+
+
